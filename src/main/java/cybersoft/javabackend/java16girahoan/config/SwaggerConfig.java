e3b8449d57b83c7559ab334d4158db932fe365ef
kothichdua12 <lb26012001@gmail.com>
2022-03-03 21:26:31 +0700

    add  swagger

---
+++ src/main/java/cybersoft/javabackend/java16girahoan/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package cybersoft.javabackend.java16girahoan.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.Contact;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket getDocket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("cybersoft.javabackend.java16girahoan"))
+				.build()
+				.apiInfo(new ApiInfoBuilder().title("Gira Application")
+						.version("1.0.0")
+						.description("This project is used for education purpose only.")
+						.contact(new Contact("123", "123", "123"))
+						.build());
+		
+	}
+}
